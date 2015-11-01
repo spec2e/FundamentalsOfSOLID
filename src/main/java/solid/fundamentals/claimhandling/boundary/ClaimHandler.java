f413cdadcfe5409089abc0151a95ae8de91e9044
zapp <obe2lix>
2015-11-01 17:38:23 +0100

    initial

--- src/main/java/solid/fundamentals/claimhandling/boundary/ClaimHandler.java
+++ src/main/java/solid/fundamentals/claimhandling/boundary/ClaimHandler.java
@@ -1,7 +1,35 @@
 package solid.fundamentals.claimhandling.boundary;
 
+import solid.fundamentals.claimhandling.control.CreateClaim;
+import solid.fundamentals.claimhandling.model.Claim;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import java.util.Date;
+
 /**
  * Created by zapp on 01/11/15.
  */
+@Stateless
 public class ClaimHandler {
+
+    @Inject
+    CreateClaim createClaim;
+
+    Claim createAutoClaim(String name,
+                           String insuredCarsLicensePlate,
+                           String counterpartsLicensePlate) {
+
+        return createClaim.execute(new Date(),
+                new Date(),
+                "AUTO",
+                name,
+                null,
+                null,
+                insuredCarsLicensePlate,
+                counterpartsLicensePlate,
+                "This is the secret information");
+
+    }
+
 }
--- src/main/java/solid/fundamentals/claimhandling/control/ViewClaim.java
+++ src/main/java/solid/fundamentals/claimhandling/control/ViewClaim.java
@@ -1,7 +1,22 @@
 package solid.fundamentals.claimhandling.control;
 
+import solid.fundamentals.claimhandling.database.PersistenceGateway;
+import solid.fundamentals.claimhandling.model.Claim;
+
+import javax.inject.Inject;
+
 /**
  * Created by zapp on 01/11/15.
  */
 public class ViewClaim {
+
+    @Inject
+    PersistenceGateway persistenceGateway;
+
+    public Claim execute(String claimId, String claimType) {
+
+        return null;
+
+    }
+
 }
